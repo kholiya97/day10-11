c3295ee8fca6380e898529887bbd816e5aa7b124
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-03-23 03:51:52 +0530

    Update shortingarithmaticcomputationproblem.sh

--- shortingarithmaticcomputationproblem.sh
+++ shortingarithmaticcomputationproblem.sh
@@ -6,5 +6,4 @@ read -p "Enter Third Input :- " c
 echo "You Enter Inputs Is :- " $a, $b, $c
 Compute_1=$((a + b * c))
 Compute_2=$((a * b + c))
-echo "Compute (a+b*c):-"$Compute_1
 echo "Compute (a*b+c):-"$Compute_1
